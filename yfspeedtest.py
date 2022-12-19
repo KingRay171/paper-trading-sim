83ed1eb6d0c4e23469e3500d036a1b1cd30e7a12
KingRay171 <raycikome@gmail.com>
2022-12-19 08:41:55 -0500

    deleted settings.json

--- yfspeedtest.py
+++ yfspeedtest.py
@@ -1,15 +1,17 @@
 import yfinance as yf
+import yahoofinancials as yahoo
 import time
 import pandas as pd
+
 t1 = time.perf_counter()
-
-spy1 = yf.Ticker("SPY").info['regularMarketPrice']
-
+spy1 = yf.Ticker("SPY").info
 t2 = time.perf_counter()
 
-spy2 = yf.download("SPY", period='1d').iloc[0][3]
-
+spy2 = yf.download("SPY", period='5d')
 t3 = time.perf_counter()
 
-print(f"yf.Ticker time: {t2 - t1}")
-print(f"yf.download time: {t3 - t2}")
+print(f"yf.Ticker: {t2 - t1}")
+print(f"yf.download: {t3 - t2}")
+
+print(spy1)
+print(spy2)

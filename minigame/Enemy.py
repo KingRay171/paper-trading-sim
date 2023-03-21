efdeddfc2a1156e3fbc6bbbaf41e9b3b503c7f04
Eric Xu <73031279+EricXu1728@users.noreply.github.com>
2023-03-21 09:05:12 -0400

    Optimization
    Creating new enemy is now optimized because it doesn't get sett.txt

--- minigame/Enemy.py
+++ minigame/Enemy.py
@@ -1,15 +1,6 @@
 import pygame
 import mathStuff
 
-file = open('minigame/sett.txt')
-  
-# read the content of the file opened
-content = file.readlines()
-
-SCREEN_WIDTH = int(content[0])
-SCREEN_HEIGHT = int(content[1])
-ENEMY_SPEED = float(content[4])
-
 class Enemy(pygame.sprite.Sprite):
     def render(self, screen):
       screen.blit(self.surf, self.rect)
@@ -19,12 +10,13 @@ class Enemy(pygame.sprite.Sprite):
       if self.hp <= 0:
           self.kill()
 
-    def __init__(self, x, y, hp ,sprite):
+    def __init__(self, x, y, hp ,sprite, speed):
         super(Enemy, self).__init__()
 
         self.hp = hp
       
         self.surf = sprite
+        self.speed = speed
       
         self.surf = pygame.transform.scale(self.surf, (60, 60))
         self.rect = self.surf.get_rect(center=(
@@ -34,15 +26,14 @@ class Enemy(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         
-        
 
     # Move the sprite based on speed
     # Remove the sprite when it passes the left edge of the screen
     def update(self, deltaTime, target):
         direction = ((target[0] - self.rect.x), (target[1] - self.rect.y))
         target = mathStuff.normalize_vector(direction)
-        self.x += target[0]* ENEMY_SPEED * deltaTime
-        self.y += target[1]* ENEMY_SPEED * deltaTime
+        self.x += target[0]* self.speed * deltaTime
+        self.y += target[1]* self.speed * deltaTime
 
         roundx = round(self.x)
         roundy = round(self.y)
